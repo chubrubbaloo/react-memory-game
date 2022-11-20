522c2487a40c1b5d53c2ab549333ddc91cda88f5
Haris Avdicevic <haris.avdi93@gmail.com>
2022-11-20 10:36:44 +0100

    Implemented logic for matching cards

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {SingleCard} from "./components/SingleCard";
 
 const cardImages = [
-    {"src": "/img/helmet-1.png"},
-    {"src": "/img/potion-1.png"},
-    {"src": "/img/ring-1.png"},
-    {"src": "/img/scroll-1.png"},
-    {"src": "/img/shield-1.png"},
-    {"src": "/img/sword-1.png"},
+    {"src": "/img/helmet-1.png", matched: false },
+    {"src": "/img/potion-1.png", matched: false },
+    {"src": "/img/ring-1.png", matched: false },
+    {"src": "/img/scroll-1.png", matched: false },
+    {"src": "/img/shield-1.png", matched: false },
+    {"src": "/img/sword-1.png", matched: false },
 ]
 
 function App() {
 
     const [cards, setCards] = useState([]);
-    const [turns,setTurns] = useState(0);
+    const [turns, setTurns] = useState(0);
+    const [choiceOne, setChoiceOne] = useState(null);
+    const [choiceTwo, setChoiceTwo] = useState(null);
 
     // Shuffles 12 cards randomly and gives each card a random id every time the on click event triggers.
     const shuffleCards = () => {
@@ -26,8 +28,41 @@ function App() {
         setTurns(0);
     }
 
-    console.log(cards,turns);
+    const handleChoice = (card) => {
+        console.log(card)
+        choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+    }
 
+    // compare 2 selected cards
+    useEffect(() => {
+        if (choiceOne && choiceTwo) {
+
+            if (choiceOne.src === choiceTwo.src) {
+                setCards(prevCards => {
+                    return prevCards.map(card => {
+                        if (card.src === choiceOne.src) {
+                            return { ...card, matched: true }
+                        } else {
+                            return card
+                        }
+                    })
+                })
+                resetTurn()
+            } else {
+                resetTurn()
+            }
+
+        }
+    }, [choiceOne, choiceTwo])
+
+    console.log(cards)
+
+    // Reset choices & increases users turn amount
+    const resetTurn = () => {
+        setChoiceOne(null);
+        setChoiceTwo(null);
+        setTurns(prevTurns => prevTurns + 1);
+    }
 
     return (
         <div className="App">
@@ -36,7 +71,11 @@ function App() {
 
             <div className="card-grid">
                 {cards.map(card => (
-                   <SingleCard key={card.id} card={card}/>
+                    <SingleCard
+                        key={card.id}
+                        card={card}
+                        handleChoice={handleChoice}
+                    />
                 ))}
             </div>
         </div>
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,12 +1,22 @@
 import './SingleCard.css'
 
-export const SingleCard = ({card}) => {
+export const SingleCard = ({card,handleChoice}) => {
+
+    const handleClick = () => {
+        handleChoice(card)
+    }
+
     return (
         <div>
             <div className="card">
                 <div>
                     <img src={card.src} className="front" alt="card front"/>
-                    <img src="/img/cover.png" className="back" alt="card back"/>
+                    <img
+                        src="/img/cover.png"
+                        className="back"
+                        alt="card back"
+                        onClick={handleClick}
+                    />
                 </div>
             </div>
         </div>
