cd1e088340a600bb83d8fdf7ac83d63613729ffa
Haris Avdicevic <haris.avdi93@gmail.com>
2022-11-20 09:20:09 +0100

    Initial structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import './App.css';
+import {useState} from "react";
+
+const cardImages = [
+    {"src": "/img/helmet-1.png"},
+    {"src": "/img/potion-1.png"},
+    {"src": "/img/ring-1.png"},
+    {"src": "/img/scroll-1.png"},
+    {"src": "/img/shield-1.png"},
+    {"src": "/img/sword-1.png"},
+]
 
 function App() {
-  return (
-      <div>
-    <h1>Hello World!</h1>
-        <button>New Game</button>
-    </div>
-  );
+
+    const [cards, setCards] = useState([]);
+    const [turns,setTurns] = useState(0);
+
+    // Shuffles 12 cards randomly and gives each card a random id every time the on click event triggers.
+    const shuffleCards = () => {
+        const shuffleCards = [...cardImages, ...cardImages]
+            .sort(() => Math.random() - 0.5)
+            .map((card) => ({...card, id: Math.random()}))
+
+        setCards(shuffleCards)
+        setTurns(0);
+    }
+
+    console.log(cards,turns);
+
+
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <button onClick={shuffleCards}>New Game</button>
+        </div>
+    );
 }
 
 export default App;
