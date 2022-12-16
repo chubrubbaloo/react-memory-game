3c6930a0d4a6fc3b1f223c17b3137a6c3aae93d7
Haris Avdicevic <haris.avdi93@gmail.com>
2022-12-16 15:15:09 +0100

    Error page fully implemented.

--- src/pages/errorPage/ErrorPage.js
+++ src/pages/errorPage/ErrorPage.js
@@ -1,5 +1,15 @@
+import error from './error.png'
+import {useNavigate} from "react-router-dom";
+
 export const ErrorPage = () => {
+    const navigate = useNavigate();
     return (
-        <div><h1>Error page</h1></div>
+        <div>
+            <img className='yatta' src={error} alt="dizzy anime person"/>
+            <h1>404</h1>
+            <h2>The page doesn't exist.</h2>
+            <p>Please check the web address or go back to the home page.</p>
+            <button onClick={() => navigate("/")}>Home</button>
+        </div>
     )
 }
