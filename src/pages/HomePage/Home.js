b4d1956d22a1d69deb82b71b9779ac50cc65464c
Haris Avdicevic <haris.avdi93@gmail.com>
2022-12-16 12:33:54 +0100

    Added some rules to the home page

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,4 +1,6 @@
 import {useNavigate} from "react-router-dom";
+import yatta from './yatta.png';
+import pics from './front-end-images.png'
 
 export const Home = () => {
 
@@ -8,6 +10,12 @@ export const Home = () => {
         <div>
             <h1>Memory Game</h1>
             <button onClick={() => navigate("/memory-game")}>Play the game!</button>
+            <img className='yatta' style={{borderRadius: '12px'}} src={yatta} alt="Anime girl"/>
+            <h2>The rules:</h2>
+            <p>There are 6 different pictures of commonly used tools in front-end development. Your goal is to guess the
+            right pair of cards in as few tries as possible!</p>
+            <br/>
+            <img className='rules-img' src={pics} alt='The 6 pictures you want to find'/>
         </div>
     )
 }
