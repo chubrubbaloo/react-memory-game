01c2758140da4158c70f93e45571a6a1aaf9716c
Haris Avdicevic <71407043+chubrubbaloo@users.noreply.github.com>
2022-12-17 17:35:21 +0100

    Update Home.js

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -12,7 +12,7 @@ export const Home = () => {
             <button onClick={() => navigate("/memory-game")}>Play the game!</button>
             <img className='yatta' style={{borderRadius: '12px'}} src={yatta} alt="Anime girl"/>
             <h2>The rules:</h2>
-            <p>There are 6 different images of commonly used tools in front-end development. Your goal is to guess the
+            <p>There are 6 different images of commonly used tools in front end development. Your goal is to guess the
             right pair of cards in as few tries as possible!</p>
             <br/>
             <img className='rules-img' src={pics} alt='The 6 pictures you want to find'/>
